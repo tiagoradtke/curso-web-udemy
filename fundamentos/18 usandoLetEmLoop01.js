6e6b9a8f71993a53bb1f2daac55e769f72473326
Tiago Radtke Quiquio <tiagoradtke@gmail.com>
2018-04-21 15:57:45 -0300

    rm fundamentos/*

--- fundamentos/18 usandoLetEmLoop01.js
+++
@@ -1,8 +0,0 @@
-const pout = (a , b) => {console.log(`${b} : ${a}`)}
-
-
-for (let i= 0; i < 10; i++){
-    pout(i,"Valor de Let dentro do loop")
-}
-
-pout(i,"Valor de Let Fora do loop")
--- fundamentos/18 usandoVarEmLoop01.js
+++
@@ -1,8 +0,0 @@
-const pout = (a , b) => console.log(`${b} : ${a}`)
-
-for (var i = 0 ;i < 10; i++){
-    pout(i, "valor de i no laço ")
-
-}
-
-pout(i, "valor de i fora do laço ")
