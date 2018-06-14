9bd450282b48304b937b5898b210e43c687f0a88
Tiago Radtke Quiquio <tiagoradtke@gmail.com>
2018-06-14 13:36:24 -0300

    Funcao IIFE, call e apply

---
+++ funcoes/17_funcaoIIFE.js
@@ -0,0 +1,7 @@
+// IIFF -> Immediately Invoked Function Expression
+
+(function(){    
+    console.log('Será executado na hora') 
+    console.log('Foge do escopo mais abrangente')       
+}    
+)()//foge do escopo global, e execu
